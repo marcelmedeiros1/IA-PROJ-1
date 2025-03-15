5085be9be0865811d8784050d42b974caa93cd51
Leonardo Garcia <leonardorsg@hotmail.com>
2025-03-15 20:15:04 +0000

    added base functions and main

---
+++ delivery/main.py
@@ -0,0 +1,13 @@
+from models import model
+from parsers import parsing 
+from algorithms import algorithm
+
+# Carregar os dados do problema
+#grid, drones, warehouses, orders, products = load_input("test_inputs/example.in")
+
+# Escolher um algoritmo de otimização
+#solution = greedy_algorithm(grid, drones, warehouses, orders, products)
+
+# Salvar a saída no formato esperado
+
+#save_output("results/solution.txt", solution)
--- delivery/models/model.py
+++ delivery/models/model.py
@@ -1,3 +1,6 @@
+import math
+from utils import utils
+
 class Location:
     def __init__(self, x: int, y: int): 
         self.x = x # Position in X-axis
@@ -28,3 +31,55 @@ class Order:
         self.location = location  # Tuple (row, col) on the grid
         self.items = items  # Dictionary {product_id: quantity}
         self.completed = False  # Whether the order has been fulfilled
+
+class Drone:
+    def __init__(self, drone_id: int, location: Location, max_payload: int):
+        self.drone_id = drone_id  # Unique drone identifier
+        self.location = location  # Tuple (row, col) on the grid
+        self.max_payload = max_payload
+        self.payload = 0
+        self.inventory = {} # Dictionary {product_id: quantity}
+        self.busy = False
+
+    def move_to(self, location: Location) -> int:
+        distance = utils.euclidean_distance(self.location, location)
+        self.location = location
+        return distance
+    
+    def load(self, warehouse: Warehouse ,product: Product, quantity: int) -> bool:
+        if product.product_id in warehouse.stock and warehouse.stock[product.product_id] >= quantity:
+            totalWeight = sum(self.inventory.get(product.product_id, 0) * product.weight for product_id in self.inventory)
+            if totalWeight + product.weight * quantity <= self.max_payload:
+                self.inventory[product.product_id] = self.inventory.get(product.product_id, 0) + quantity
+                self.payload += product.weight * quantity
+                warehouse.stock[product.product_id] -= quantity
+                return True
+        return False
+    
+    def deliver(self, order: Order, product: Product, quantity: int) -> bool:
+        if product.product_id in self.inventory and self.inventory[product.product_id] >= quantity:
+            self.inventory[product.product_id] -= quantity
+            self.payload -= product.weight * quantity
+            order.items[product.product_id] -= quantity
+            if(order.items[product.product_id] == 0):
+                order.items.pop(product.product_id)
+            if not order.items:
+                order.completed = True
+            return True
+        return False
+    
+class Simulation:
+    def __init__(self, grid: Grid, products: list ,drones: list, warehouses: list, orders: list):
+        self.grid = grid
+        self.drones = drones
+        self.products = products
+        self.warehouses = warehouses
+        self.orders = orders
+        self.time = 0
+
+    def run(self):
+        """
+        Run the simulation
+        """
+        pass # Implement algorithms and call them here
+        
