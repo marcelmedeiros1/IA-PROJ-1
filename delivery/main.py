184c4283c1a72b6b0a65e9d7e11bb690c9e9cb2a
“manoela.americo” <manoela.americo@inesctec.com>
2025-04-06 16:02:29 +0100

    Implement genetic algorithm for optimization;

--- delivery/main.py
+++ delivery/main.py
@@ -1,34 +1,20 @@
 from models.model import *
 from parsers.parsing import * 
-from algorithms.algorithm import *
+from algorithms.genetics1 import *
 from utils.utils import *
 import copy
 
-# Carregar os dados do problema
-#grid, drones, warehouses, orders, products = load_input("test_inputs/example.in")
+def main():
+    data = parse_file("mother_of_all_warehouses.in")   
+    sim = Simulation(data)
 
-# Escolher um algoritmo de otimização
-#solution = greedy_algorithm(grid, drones, warehouses, orders, products)
+    ga = GeneticAlgorithm(sim, population_size=2, num_generations=50,crossover_rate=0.7 ,mutation_rate=0.3)
 
-# Salvar a saída no formato esperado
+    best_chrom, best_score = ga.run(sim)
+    print("\n=== GA run complete ===")
+    print("Best Score:", best_score)
+    ga.print_solution()
 
-#save_output("results/solution.txt", solution)
+if __name__ == "__main__":
+    main()
 
-# Testing Parsing
-# python3 main.py > parsing_test.txt (command to execute)
-
-
-# Testing Brute-Force
-simulation_data = parse_file("tiny.in")
-simulation_o = Simulation(simulation_data)
-simulation = copy.deepcopy(simulation_o)
-total_score = simulate_turns(simulation.drones, simulation.warehouses, simulation.orders, simulation.products, simulation.deadline)
-print(total_score)
-
-
-# simulation_data = parse_file("busy_day.in")
-# simulation = Simulation(simulation_data)
-# # Testing Algorithm
-# aco = AntColonyOpt(simulation.grid, simulation.drones, simulation.warehouses, simulation.orders, simulation.products, 10)
-# solution = aco.run()
-# aco.print_solution()
